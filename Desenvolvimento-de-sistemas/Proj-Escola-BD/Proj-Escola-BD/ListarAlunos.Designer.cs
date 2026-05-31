3c9ce1ccd93555ff100877ef4b9061cefac95a64
VALTER SERGIO RIBEIRO TERTULIANO <valtertert@gmail.com>
2026-05-31 15:00:37 -0300

    Filtros da tela de listagem concluidas

--- Desenvolvimento-de-sistemas/Proj-Escola-BD/Proj-Escola-BD/ListarAlunos.Designer.cs
+++ Desenvolvimento-de-sistemas/Proj-Escola-BD/Proj-Escola-BD/ListarAlunos.Designer.cs
@@ -32,23 +32,32 @@
             this.outputFoto = new System.Windows.Forms.PictureBox();
             this.btnListarAlunos = new System.Windows.Forms.Button();
             this.btnAniversariantes = new System.Windows.Forms.Button();
-            this.inputData = new System.Windows.Forms.DateTimePicker();
+            this.dataAtual = new System.Windows.Forms.DateTimePicker();
+            this.btnDesconto = new System.Windows.Forms.Button();
+            this.btnPagamentos = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputDataDe = new System.Windows.Forms.DateTimePicker();
+            this.inputDataAte = new System.Windows.Forms.DateTimePicker();
+            this.lblDataDe = new System.Windows.Forms.Label();
+            this.lblDataAte = new System.Windows.Forms.Label();
+            this.checkFiltro = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.gridAlunos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.outputFoto)).BeginInit();
             this.SuspendLayout();
             // 
             // gridAlunos
             // 
+            this.gridAlunos.BackgroundColor = System.Drawing.SystemColors.WindowText;
             this.gridAlunos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.gridAlunos.Location = new System.Drawing.Point(12, 33);
             this.gridAlunos.Name = "gridAlunos";
-            this.gridAlunos.Size = new System.Drawing.Size(537, 405);
+            this.gridAlunos.Size = new System.Drawing.Size(765, 470);
             this.gridAlunos.TabIndex = 0;
             this.gridAlunos.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gridAlunos_MouseClick);
             // 
             // outputFoto
             // 
-            this.outputFoto.Location = new System.Drawing.Point(593, 33);
+            this.outputFoto.Location = new System.Drawing.Point(799, 33);
             this.outputFoto.Name = "outputFoto";
             this.outputFoto.Size = new System.Drawing.Size(195, 176);
             this.outputFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -57,7 +66,7 @@
             // 
             // btnListarAlunos
             // 
-            this.btnListarAlunos.Location = new System.Drawing.Point(593, 234);
+            this.btnListarAlunos.Location = new System.Drawing.Point(799, 225);
             this.btnListarAlunos.Name = "btnListarAlunos";
             this.btnListarAlunos.Size = new System.Drawing.Size(195, 23);
             this.btnListarAlunos.TabIndex = 2;
@@ -67,7 +76,7 @@
             // 
             // btnAniversariantes
             // 
-            this.btnAniversariantes.Location = new System.Drawing.Point(593, 286);
+            this.btnAniversariantes.Location = new System.Drawing.Point(799, 254);
             this.btnAniversariantes.Name = "btnAniversariantes";
             this.btnAniversariantes.Size = new System.Drawing.Size(195, 23);
             this.btnAniversariantes.TabIndex = 3;
@@ -75,20 +84,107 @@
             this.btnAniversariantes.UseVisualStyleBackColor = true;
             this.btnAniversariantes.Click += new System.EventHandler(this.btnAniversariantes_Click);
             // 
-            // inputData
+            // dataAtual
             // 
-            this.inputData.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.inputData.Location = new System.Drawing.Point(593, 263);
-            this.inputData.Name = "inputData";
-            this.inputData.Size = new System.Drawing.Size(200, 20);
-            this.inputData.TabIndex = 4;
+            this.dataAtual.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dataAtual.Location = new System.Drawing.Point(82, 7);
+            this.dataAtual.Name = "dataAtual";
+            this.dataAtual.Size = new System.Drawing.Size(195, 20);
+            this.dataAtual.TabIndex = 4;
+            // 
+            // btnDesconto
+            // 
+            this.btnDesconto.Location = new System.Drawing.Point(799, 397);
+            this.btnDesconto.Name = "btnDesconto";
+            this.btnDesconto.Size = new System.Drawing.Size(195, 23);
+            this.btnDesconto.TabIndex = 6;
+            this.btnDesconto.Text = "Descontos do Mês";
+            this.btnDesconto.UseVisualStyleBackColor = true;
+            this.btnDesconto.Click += new System.EventHandler(this.btnDesconto_Click);
+            // 
+            // btnPagamentos
+            // 
+            this.btnPagamentos.Location = new System.Drawing.Point(799, 368);
+            this.btnPagamentos.Name = "btnPagamentos";
+            this.btnPagamentos.Size = new System.Drawing.Size(195, 23);
+            this.btnPagamentos.TabIndex = 7;
+            this.btnPagamentos.Text = "Pagamentos do Mês";
+            this.btnPagamentos.UseVisualStyleBackColor = true;
+            this.btnPagamentos.Click += new System.EventHandler(this.btnPagamentos_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Data Atual: ";
+            // 
+            // inputDataDe
+            // 
+            this.inputDataDe.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.inputDataDe.Location = new System.Drawing.Point(838, 306);
+            this.inputDataDe.Name = "inputDataDe";
+            this.inputDataDe.Size = new System.Drawing.Size(156, 20);
+            this.inputDataDe.TabIndex = 9;
+            this.inputDataDe.Visible = false;
+            // 
+            // inputDataAte
+            // 
+            this.inputDataAte.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.inputDataAte.Location = new System.Drawing.Point(839, 332);
+            this.inputDataAte.Name = "inputDataAte";
+            this.inputDataAte.Size = new System.Drawing.Size(155, 20);
+            this.inputDataAte.TabIndex = 10;
+            this.inputDataAte.Visible = false;
+            // 
+            // lblDataDe
+            // 
+            this.lblDataDe.AutoSize = true;
+            this.lblDataDe.Location = new System.Drawing.Point(796, 312);
+            this.lblDataDe.Name = "lblDataDe";
+            this.lblDataDe.Size = new System.Drawing.Size(27, 13);
+            this.lblDataDe.TabIndex = 11;
+            this.lblDataDe.Text = "De: ";
+            this.lblDataDe.Visible = false;
+            // 
+            // lblDataAte
+            // 
+            this.lblDataAte.AutoSize = true;
+            this.lblDataAte.Location = new System.Drawing.Point(795, 338);
+            this.lblDataAte.Name = "lblDataAte";
+            this.lblDataAte.Size = new System.Drawing.Size(28, 13);
+            this.lblDataAte.TabIndex = 12;
+            this.lblDataAte.Text = "até: ";
+            this.lblDataAte.Visible = false;
+            // 
+            // checkFiltro
+            // 
+            this.checkFiltro.AutoSize = true;
+            this.checkFiltro.Location = new System.Drawing.Point(799, 283);
+            this.checkFiltro.Name = "checkFiltro";
+            this.checkFiltro.Size = new System.Drawing.Size(95, 17);
+            this.checkFiltro.TabIndex = 13;
+            this.checkFiltro.Text = "Filtrar por Data";
+            this.checkFiltro.UseVisualStyleBackColor = true;
+            this.checkFiltro.CheckedChanged += new System.EventHandler(this.checkFiltro_CheckedChanged);
             // 
             // ListarAlunos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.inputData);
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1018, 515);
+            this.Controls.Add(this.checkFiltro);
+            this.Controls.Add(this.lblDataAte);
+            this.Controls.Add(this.lblDataDe);
+            this.Controls.Add(this.inputDataAte);
+            this.Controls.Add(this.inputDataDe);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPagamentos);
+            this.Controls.Add(this.btnDesconto);
+            this.Controls.Add(this.dataAtual);
             this.Controls.Add(this.btnAniversariantes);
             this.Controls.Add(this.btnListarAlunos);
             this.Controls.Add(this.outputFoto);
@@ -98,6 +194,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.gridAlunos)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.outputFoto)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -107,6 +204,14 @@
         private System.Windows.Forms.PictureBox outputFoto;
         private System.Windows.Forms.Button btnListarAlunos;
         private System.Windows.Forms.Button btnAniversariantes;
-        private System.Windows.Forms.DateTimePicker inputData;
+        private System.Windows.Forms.DateTimePicker dataAtual;
+        private System.Windows.Forms.Button btnDesconto;
+        private System.Windows.Forms.Button btnPagamentos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker inputDataDe;
+        private System.Windows.Forms.DateTimePicker inputDataAte;
+        private System.Windows.Forms.Label lblDataDe;
+        private System.Windows.Forms.Label lblDataAte;
+        private System.Windows.Forms.CheckBox checkFiltro;
     }
 }
--- Desenvolvimento-de-sistemas/Proj-Escola-BD/Proj-Escola-BD/ListarAlunos.cs
+++ Desenvolvimento-de-sistemas/Proj-Escola-BD/Proj-Escola-BD/ListarAlunos.cs
@@ -61,7 +61,7 @@ namespace Proj_Escola_BD
         private void btnAniversariantes_Click(object sender, EventArgs e)
         {
             // capturar o mês informado
-            int mes = inputData.Value.Month;
+            int mes = dataAtual.Value.Month;
 
             // comando para obter dados de alunos
             comando_sql.Remove(0, comando_sql.Length);
@@ -93,6 +93,7 @@ namespace Proj_Escola_BD
 
         }
 
+
         private void exibirFoto()
         {
             try
@@ -112,6 +113,135 @@ namespace Proj_Escola_BD
                 outputFoto.Image = null;
 
             }
+        }
+
+        private void btnPagamentos_Click(object sender, EventArgs e)
+        {
+
+            comando_sql.Remove(0, comando_sql.Length);
+            comando_sql.Append("SELECT ");
+
+            // modificando o nome das colunas de apresentação
+            comando_sql.Append("alunos.Matricula_Alu as 'Matricula', ");
+            comando_sql.Append("Nome_Alu as 'Nome', ");
+            comando_sql.Append("Email_Alu as 'Email', ");
+            comando_sql.Append("Nasc_Alu as 'Data de nascimento', ");
+            comando_sql.Append("CPF_Alu as 'CPF', ");
+            comando_sql.Append("Foto_Alu as 'Foto Aluno', ");
+            comando_sql.Append("DtPag_Mens as 'Data de Pagamento', ");
+            comando_sql.Append("VlPag_Mens as 'Valor do pagamento', ");
+            comando_sql.Append("Juros_Mens as 'Juros por atraso', ");
+            comando_sql.Append("Desconto_Mens as 'Desconto por adiantamento', ");
+
+            // condição para Juros pago
+            comando_sql.Append("IF (Juros_Mens > 0 && Desconto_Mens = 0, ");
+
+            // calcula o valor a ser pago na mensalidade 
+            comando_sql.Append("VlPag_Mens + (VlPag_Mens * (Juros_Mens / 100)),");
+
+            // condição para desconto pago
+            comando_sql.Append("IF (Desconto_Mens > 0 && Juros_Mens = 0, ");
+
+            // calcular o valor a ser pago
+            comando_sql.Append("VlPag_Mens - (VlPag_Mens * (Desconto_Mens / 100)), ");
+
+            // define um alias para o resultado do calculo
+            comando_sql.Append("VlPag_Mens)) as 'Valor da mensalidade' ");
+
+            // associa as tabelas para consulta de pagamento
+            comando_sql.Append("FROM mensalidade "); // Primeira tabela para associação 
+            comando_sql.Append("INNER JOIN Alunos "); // Segunda tabela para associação
+            comando_sql.Append("ON mensalidade.Matricula_Alu = alunos.Matricula_Alu "); // condição para associção
+
+            // verifica se o botão de radio foi selecionado
+            if (checkFiltro.Checked)
+            {
+                comando_sql.Append("WHERE DtPag_Mens >= @DataDe AND DtPag_Mens < @DataAte");
+
+                conexao.Comandos.Parameters.Clear();
+                conexao.Comandos.Parameters.AddWithValue("@DataDe", new DateTime(inputDataDe.Value.Year, inputDataDe.Value.Month, inputDataDe.Value.Day));
+                conexao.Comandos.Parameters.AddWithValue("@DataAte", new DateTime(inputDataAte.Value.Year, inputDataAte.Value.Month, inputDataAte.Value.Day));
+            }
+            else
+            {
+
+                comando_sql.Append("where Month(DtPag_Mens) = @DtPag_Mens;");
+                conexao.Comandos.Parameters.Clear(); // limpa os parametros da conexão
+                conexao.Comandos.Parameters.AddWithValue("@DtPag_Mens", dataAtual.Value.Month);
+            }
+
+            // envia o comando para execução
+            conexao.StrSql = comando_sql.ToString();
+            conjunto_dados = conexao.RetornarDataSet();
+
+            // envia os dados para tabela do grid
+            tabela_dados = conjunto_dados.Tables[0];
+            gridAlunos.DataSource = tabela_dados;
+
+        }
+
+        private void checkFiltro_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkFiltro.Checked)
+            {
+                lblDataAte.Visible = true;
+                lblDataDe.Visible = true;
+                inputDataAte.Visible = true;
+                inputDataDe.Visible = true;
+            } else
+            {
+                lblDataAte.Visible = false;
+                lblDataDe.Visible= false;
+                inputDataAte.Visible = false;
+                inputDataDe.Visible = false;
+            }
+        }
+
+        private void btnDesconto_Click(object sender, EventArgs e)
+        {
+            comando_sql.Remove(0, comando_sql.Length);
+            comando_sql.Append("SELECT ");
+
+            // modificando o nome das colunas de apresentação
+            comando_sql.Append("alunos.Matricula_Alu as 'Matricula', ");
+            comando_sql.Append("Nome_Alu as 'Nome', ");
+            comando_sql.Append("Email_Alu as 'Email', ");
+            comando_sql.Append("Nasc_Alu as 'Data de nascimento', ");
+            comando_sql.Append("CPF_Alu as 'CPF', ");
+            comando_sql.Append("Foto_Alu as 'Foto Aluno', ");
+            comando_sql.Append("DtPag_Mens as 'Data de Pagamento', ");
+            comando_sql.Append("VlPag_Mens as 'Valor do pagamento', ");
+            comando_sql.Append("Desconto_Mens as 'Desconto por adiantamento' ");
+            // associa as tabelas para consulta de pagamento
+            comando_sql.Append("FROM mensalidade "); // Primeira tabela para associação 
+            comando_sql.Append("INNER JOIN alunos "); // Segunda tabela para associação
+            comando_sql.Append("ON mensalidade.Matricula_Alu = alunos.Matricula_Alu "); // condição para associção
+            // verifica se o botão de radio foi selecionado
+            if (checkFiltro.Checked)
+            {
+                comando_sql.Append("WHERE Desconto_Mens > 0 "); // condição que garante apenas os que tem desconto
+                comando_sql.Append("AND DtPag_Mens >= @DataDe AND DtPag_Mens <= @DataAte "); // garante apenas os registros dentro do intervalo de datas
+                comando_sql.Append("ORDER by Nome_Alu, DtPag_Mens;"); // ordena pelo nome e pela data
+                conexao.Comandos.Parameters.Clear();
+                conexao.Comandos.Parameters.AddWithValue("@DataDe", new DateTime(inputDataDe.Value.Year, inputDataDe.Value.Month, inputDataDe.Value.Day));
+                conexao.Comandos.Parameters.AddWithValue("@DataAte", new DateTime(inputDataAte.Value.Year, inputDataAte.Value.Month, inputDataAte.Value.Day));
+            }
+            else
+            {
+
+                comando_sql.Append("WHERE Desconto_Mens > 0 AND  Month(DtPag_Mens) = @Mes AND Year(DtPag_Mens) = @Ano ORDER by Nome_Alu, DtPag_Mens;");
+                conexao.Comandos.Parameters.Clear(); // limpa os parametros da conexão
+                conexao.Comandos.Parameters.AddWithValue("@Mes", dataAtual.Value.Month);
+                conexao.Comandos.Parameters.AddWithValue("@Ano", dataAtual.Value.Year);
+            }
+
+            // envia o comando para execução
+            conexao.StrSql = comando_sql.ToString();
+            conjunto_dados = conexao.RetornarDataSet();
+
+            // envia os dados para tabela do grid
+            tabela_dados = conjunto_dados.Tables[0];
+            gridAlunos.DataSource = tabela_dados;
         }
     }
 }
