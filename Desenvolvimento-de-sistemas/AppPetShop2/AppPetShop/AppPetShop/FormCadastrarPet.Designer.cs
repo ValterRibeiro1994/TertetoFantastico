80544816a9ab6be760a2307e41be9c7dfab5646a
VALTER SERGIO RIBEIRO TERTULIANO <valtertert@gmail.com>
2026-06-16 19:43:26 -0300

    Update FormCadastrarPet.Designer.cs

--- Desenvolvimento-de-sistemas/AppPetShop2/AppPetShop/AppPetShop/FormCadastrarPet.Designer.cs
+++ Desenvolvimento-de-sistemas/AppPetShop2/AppPetShop/AppPetShop/FormCadastrarPet.Designer.cs
@@ -54,6 +54,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(335, 55);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(267, 202);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -61,16 +62,16 @@
             // 
             this.button1.Location = new System.Drawing.Point(12, 281);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(281, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(439, 281);
+            this.button2.Location = new System.Drawing.Point(335, 281);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(267, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
