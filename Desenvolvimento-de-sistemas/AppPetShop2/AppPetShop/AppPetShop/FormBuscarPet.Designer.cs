2c54cde39274c542c9d6b0c01acb9604490da3c1
VALTER SERGIO RIBEIRO TERTULIANO <valtertert@gmail.com>
2026-06-19 08:03:14 -0300

    update tela buscar pet

--- Desenvolvimento-de-sistemas/AppPetShop2/AppPetShop/AppPetShop/FormBuscarPet.Designer.cs
+++ Desenvolvimento-de-sistemas/AppPetShop2/AppPetShop/AppPetShop/FormBuscarPet.Designer.cs
@@ -49,6 +49,7 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -125,9 +126,9 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(16, 229);
+            this.pictureBox1.Location = new System.Drawing.Point(528, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(253, 146);
+            this.pictureBox1.Size = new System.Drawing.Size(275, 213);
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             // 
@@ -177,14 +178,14 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(399, 10);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 223);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(513, 363);
+            this.dataGridView1.Size = new System.Drawing.Size(791, 325);
             this.dataGridView1.TabIndex = 15;
             // 
             // btnBuscarPedCod
             // 
-            this.btnBuscarPedCod.Location = new System.Drawing.Point(275, 258);
+            this.btnBuscarPedCod.Location = new System.Drawing.Point(403, 51);
             this.btnBuscarPedCod.Name = "btnBuscarPedCod";
             this.btnBuscarPedCod.Size = new System.Drawing.Size(109, 23);
             this.btnBuscarPedCod.TabIndex = 16;
@@ -194,7 +195,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(275, 287);
+            this.button2.Location = new System.Drawing.Point(403, 80);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(109, 23);
             this.button2.TabIndex = 17;
@@ -203,36 +204,46 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(275, 316);
+            this.button3.Location = new System.Drawing.Point(403, 192);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(109, 23);
             this.button3.TabIndex = 18;
-            this.button3.Text = "Editar Pet";
+            this.button3.Text = "Alterar Foto";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(275, 352);
+            this.button4.Location = new System.Drawing.Point(403, 132);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(109, 23);
+            this.button4.Size = new System.Drawing.Size(109, 22);
             this.button4.TabIndex = 19;
             this.button4.Text = "Remover Pet";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(275, 229);
+            this.button5.Location = new System.Drawing.Point(403, 12);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(109, 23);
             this.button5.TabIndex = 20;
             this.button5.Text = "Listar Pet";
             this.button5.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(403, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Editar Pet";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // FormBuscarPet
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(924, 387);
+            this.ClientSize = new System.Drawing.Size(1010, 548);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -286,5 +297,6 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
     }
 }
