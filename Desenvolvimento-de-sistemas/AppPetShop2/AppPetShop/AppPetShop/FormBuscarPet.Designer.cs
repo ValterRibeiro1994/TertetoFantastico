e58e338a9625837c9d4ff7ec7f9a62ecf87145de
VALTER SERGIO RIBEIRO TERTULIANO <valtertert@gmail.com>
2026-06-19 18:37:13 -0300

    conflito no merge

---
+++ Desenvolvimento-de-sistemas/AppPetShop2/AppPetShop/AppPetShop/FormBuscarPet.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace AppPetShop
+{
+    partial class FormBuscarPet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.campoCodigo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.campoImagem = new System.Windows.Forms.PictureBox();
+            this.campoCpf = new System.Windows.Forms.TextBox();
+            this.campoNome = new System.Windows.Forms.TextBox();
+            this.campoEspecie = new System.Windows.Forms.TextBox();
+            this.campoRaca = new System.Windows.Forms.TextBox();
+            this.campoData = new System.Windows.Forms.DateTimePicker();
+            this.gridPet = new System.Windows.Forms.DataGridView();
+            this.btnBuscarPedCod = new System.Windows.Forms.Button();
+            this.btnBuscarCpf = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnRemoverPet = new System.Windows.Forms.Button();
+            this.btnListarPet = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rbMacho = new System.Windows.Forms.RadioButton();
+            this.rbFemea = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.campoImagem)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridPet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código Pet";
+            // 
+            // campoCodigo
+            // 
+            this.campoCodigo.Location = new System.Drawing.Point(122, 10);
+            this.campoCodigo.Name = "campoCodigo";
+            this.campoCodigo.Size = new System.Drawing.Size(262, 20);
+            this.campoCodigo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cpf Tutor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nome Pet";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Especie Pet";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Raça Pet";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Gênero Pet";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(18, 197);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Nascimento Pet";
+            // 
+            // campoImagem
+            // 
+            this.campoImagem.Location = new System.Drawing.Point(528, 4);
+            this.campoImagem.Name = "campoImagem";
+            this.campoImagem.Size = new System.Drawing.Size(380, 213);
+            this.campoImagem.TabIndex = 8;
+            this.campoImagem.TabStop = false;
+            // 
+            // campoCpf
+            // 
+            this.campoCpf.Location = new System.Drawing.Point(122, 39);
+            this.campoCpf.Name = "campoCpf";
+            this.campoCpf.Size = new System.Drawing.Size(262, 20);
+            this.campoCpf.TabIndex = 9;
+            // 
+            // campoNome
+            // 
+            this.campoNome.Location = new System.Drawing.Point(122, 69);
+            this.campoNome.Name = "campoNome";
+            this.campoNome.Size = new System.Drawing.Size(262, 20);
+            this.campoNome.TabIndex = 10;
+            // 
+            // campoEspecie
+            // 
+            this.campoEspecie.Location = new System.Drawing.Point(122, 102);
+            this.campoEspecie.Name = "campoEspecie";
+            this.campoEspecie.Size = new System.Drawing.Size(262, 20);
+            this.campoEspecie.TabIndex = 11;
+            // 
+            // campoRaca
+            // 
+            this.campoRaca.Location = new System.Drawing.Point(122, 130);
+            this.campoRaca.Name = "campoRaca";
+            this.campoRaca.Size = new System.Drawing.Size(262, 20);
+            this.campoRaca.TabIndex = 12;
+            // 
+            // campoData
+            // 
+            this.campoData.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.campoData.Location = new System.Drawing.Point(122, 197);
+            this.campoData.Name = "campoData";
+            this.campoData.Size = new System.Drawing.Size(262, 20);
+            this.campoData.TabIndex = 14;
+            // 
+            // gridPet
+            // 
+            this.gridPet.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridPet.Location = new System.Drawing.Point(12, 223);
+            this.gridPet.Name = "gridPet";
+            this.gridPet.Size = new System.Drawing.Size(896, 325);
+            this.gridPet.TabIndex = 15;
+            this.gridPet.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.selecionarPet);
+            // 
+            // btnBuscarPedCod
+            // 
+            this.btnBuscarPedCod.Location = new System.Drawing.Point(403, 51);
+            this.btnBuscarPedCod.Name = "btnBuscarPedCod";
+            this.btnBuscarPedCod.Size = new System.Drawing.Size(109, 23);
+            this.btnBuscarPedCod.TabIndex = 16;
+            this.btnBuscarPedCod.Text = "Buscar Pet Codigo";
+            this.btnBuscarPedCod.UseVisualStyleBackColor = true;
+            this.btnBuscarPedCod.Click += new System.EventHandler(this.btnBuscarPedCod_Click);
+            // 
+            // btnBuscarCpf
+            // 
+            this.btnBuscarCpf.Location = new System.Drawing.Point(403, 80);
+            this.btnBuscarCpf.Name = "btnBuscarCpf";
+            this.btnBuscarCpf.Size = new System.Drawing.Size(109, 23);
+            this.btnBuscarCpf.TabIndex = 17;
+            this.btnBuscarCpf.Text = "Buscar Pet Cpf";
+            this.btnBuscarCpf.UseVisualStyleBackColor = true;
+            this.btnBuscarCpf.Click += new System.EventHandler(this.btnBuscarCpf_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(403, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Alterar Foto";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnRemoverPet
+            // 
+            this.btnRemoverPet.Location = new System.Drawing.Point(403, 132);
+            this.btnRemoverPet.Name = "btnRemoverPet";
+            this.btnRemoverPet.Size = new System.Drawing.Size(109, 22);
+            this.btnRemoverPet.TabIndex = 19;
+            this.btnRemoverPet.Text = "Remover Pet";
+            this.btnRemoverPet.UseVisualStyleBackColor = true;
+<<<<<<< HEAD
+=======
+            this.btnRemoverPet.Click += new System.EventHandler(this.btnRemoverPet_Click);
+>>>>>>> 721bc463d15ec9b829ed5778425b8dbd7a6beac2
+            // 
+            // btnListarPet
+            // 
+            this.btnListarPet.Location = new System.Drawing.Point(403, 12);
+            this.btnListarPet.Name = "btnListarPet";
+            this.btnListarPet.Size = new System.Drawing.Size(109, 23);
+            this.btnListarPet.TabIndex = 20;
+            this.btnListarPet.Text = "Listar Pet";
+            this.btnListarPet.UseVisualStyleBackColor = true;
+            this.btnListarPet.Click += new System.EventHandler(this.btnListarPet_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(403, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Editar Pet";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // rbMacho
+            // 
+            this.rbMacho.AutoSize = true;
+            this.rbMacho.Location = new System.Drawing.Point(122, 163);
+            this.rbMacho.Name = "rbMacho";
+            this.rbMacho.Size = new System.Drawing.Size(58, 17);
+            this.rbMacho.TabIndex = 22;
+            this.rbMacho.TabStop = true;
+            this.rbMacho.Text = "Macho";
+            this.rbMacho.UseVisualStyleBackColor = true;
+            // 
+            // rbFemea
+            // 
+            this.rbFemea.AutoSize = true;
+            this.rbFemea.Location = new System.Drawing.Point(248, 163);
+            this.rbFemea.Name = "rbFemea";
+            this.rbFemea.Size = new System.Drawing.Size(57, 17);
+            this.rbFemea.TabIndex = 23;
+            this.rbFemea.TabStop = true;
+            this.rbFemea.Text = "Femea";
+            this.rbFemea.UseVisualStyleBackColor = true;
+            // 
+            // FormBuscarPet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 548);
+            this.Controls.Add(this.rbFemea);
+            this.Controls.Add(this.rbMacho);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnListarPet);
+            this.Controls.Add(this.btnRemoverPet);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnBuscarCpf);
+            this.Controls.Add(this.btnBuscarPedCod);
+            this.Controls.Add(this.gridPet);
+            this.Controls.Add(this.campoData);
+            this.Controls.Add(this.campoRaca);
+            this.Controls.Add(this.campoEspecie);
+            this.Controls.Add(this.campoNome);
+            this.Controls.Add(this.campoCpf);
+            this.Controls.Add(this.campoImagem);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.campoCodigo);
+            this.Controls.Add(this.label1);
+            this.Name = "FormBuscarPet";
+            this.Text = "FormBuscarPet";
+            ((System.ComponentModel.ISupportInitialize)(this.campoImagem)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridPet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox campoCodigo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox campoImagem;
+        private System.Windows.Forms.TextBox campoCpf;
+        private System.Windows.Forms.TextBox campoNome;
+        private System.Windows.Forms.TextBox campoEspecie;
+        private System.Windows.Forms.TextBox campoRaca;
+        private System.Windows.Forms.DateTimePicker campoData;
+        private System.Windows.Forms.DataGridView gridPet;
+        private System.Windows.Forms.Button btnBuscarPedCod;
+        private System.Windows.Forms.Button btnBuscarCpf;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnRemoverPet;
+        private System.Windows.Forms.Button btnListarPet;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton rbMacho;
+        private System.Windows.Forms.RadioButton rbFemea;
+    }
+}
---
+++ Desenvolvimento-de-sistemas/AppPetShop2/AppPetShop/AppPetShop/FormBuscarPet.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppPetShop
+{
+    public partial class FormBuscarPet : Form
+    {
+        RepositorioPet repositorio;
+        public FormBuscarPet()
+        {
+            InitializeComponent();
+            repositorio = new RepositorioPet();
+        }
+
+        private void btnBuscarPedCod_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CodigoBanco codigo_pet = new CodigoBanco(campoCodigo.Text);
+                Pet pet = repositorio.buscarPetCodigo(codigo_pet);
+                if (pet == null)
+                {
+                    MessageBox.Show("Pet não Localizado");
+                    return;
+                }
+                MessageBox.Show("Pet Localizado");
+                enviarDados(pet);
+
+                
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+    }
+
+        private void btnBuscarCpf_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Cpf cpf = new Cpf(campoCpf.Text);
+                repositorio.buscarPetCpf(cpf, gridPet);
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void enviarDados(Pet pet) {
+            campoCodigo.Text = pet.getCodigo().ToString();
+            campoCpf.Text = pet.getCpf();
+            campoData.Value = pet.getNascimento();
+            campoEspecie.Text = pet.getEspecie();
+            campoNome.Text = pet.getNome();
+            campoRaca.Text = pet.getRaca();
+            char genero = pet.getGenero();
+            if (genero.Equals('F'))
+            {
+                rbFemea.Checked = true;
+            } else
+            {
+                rbMacho.Checked = true;
+            }
+            using (MemoryStream ms = new MemoryStream(pet.getFoto()))
+            {
+                Image img_pet = Image.FromStream(ms);
+
+                campoImagem.Image = img_pet;
+            }
+        }
+
+        private void btnListarPet_Click(object sender, EventArgs e)
+        {
+<<<<<<< HEAD
+            try
+            {
+                repositorio.listarPet(gridPet);
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void selecionarPet(object sender, DataGridViewCellEventArgs e)
+        {
+            campoCodigo.Text = gridPet.CurrentRow.Cells[0].Value.ToString();
+            campoCpf.Text = gridPet.CurrentRow.Cells[1].Value.ToString();
+            campoEspecie.Text = gridPet.CurrentRow.Cells[6].Value.ToString();
+            campoRaca.Text = gridPet.CurrentRow.Cells[4].Value.ToString();
+            campoNome.Text = gridPet.CurrentRow.Cells[5].Value.ToString();
+            campoData.Text = gridPet.CurrentRow.Cells[2].Value.ToString();
+            string genero = gridPet.CurrentRow.Cells[3].Value.ToString();
+            if (genero.Equals("F"))
+            {
+                rbFemea.Checked = true;
+            } else if (genero.Equals("M"))
+            {
+                rbMacho.Checked = true;
+            } else
+            {
+                rbMacho.Checked = false;
+                rbFemea.Checked = false;
+            }
+            byte[] blob_pet = (byte[])gridPet.CurrentRow.Cells[7].Value;
+
+            using (MemoryStream ms = new MemoryStream(blob_pet))
+            {
+                Image img_pet = Image.FromStream(ms);
+
+                campoImagem.Image = img_pet;
+            }
+
+=======
+            repositorio.buscarPets(gridPet);
+        }
+
+        private void btnRemoverPet_Click(object sender, EventArgs e)
+        {
+            
+>>>>>>> 721bc463d15ec9b829ed5778425b8dbd7a6beac2
+        }
+    }
+}
